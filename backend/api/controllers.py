0278bcdb4d351d313b6bdc32d41ee90c00888dd3
jfranco1701 <jfranco@unomaha.edu>
2018-09-24 18:59:15 -0500

    Breed Detail Complete

--- backend/api/controllers.py
+++ backend/api/controllers.py
@@ -143,12 +143,37 @@ class BreedList(APIView):
     def post(self, request, format=None):
         print 'REQUEST DATA'
         print str(request.data)
-        
+
         serializer = BreedSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class BreedDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Breed.objects.get(pk=pk)
+        except Breed.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        breed = self.get_object(pk)
+        serializer = BreedSerializer(breed)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        breed = self.get_object(pk)
+        serializer = BreedSerializer(breed, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        breed = self.get_object(pk)
+        breed.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class DogList(APIView):
     def get(self, request, format=None):
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     url(r'^events', csrf_exempt(controllers.Events.as_view())),
     url(r'^activateifttt', csrf_exempt(controllers.ActivateIFTTT.as_view())),
     url(r'^dog', csrf_exempt(controllers.DogList.as_view())),
+    url(r'^breeds/(?P<pk>[0-9]+)', csrf_exempt(controllers.BreedDetail.as_view())),
     url(r'^breeds', csrf_exempt(controllers.BreedList.as_view())),
     url(r'^', include(router.urls)),
 ]
