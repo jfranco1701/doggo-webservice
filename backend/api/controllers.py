c2f9e7d59723c5ae3b5ee1b82035d37b4fd03c4a
jfranco1701 <jfranco@unomaha.edu>
2018-09-24 19:04:18 -0500

    Updated DogDetail Class View

--- backend/api/controllers.py
+++ backend/api/controllers.py
@@ -191,24 +191,24 @@ class DogList(APIView):
 class DogDetail(APIView):
     def get_object(self, pk):
         try:
-            return Snippet.objects.get(pk=pk)
-        except Snippet.DoesNotExist:
+            return Dog.objects.get(pk=pk)
+        except Dog.DoesNotExist:
             raise Http404
 
     def get(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet)
+        dog = self.get_object(pk)
+        serializer = DogSerializer(dog)
         return Response(serializer.data)
 
     def put(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet, data=request.data)
+        dog = self.get_object(pk)
+        serializer = DogSerializer(dog, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        snippet.delete()
+        dog = self.get_object(pk)
+        dog.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
